917e87cd13f97260898e0b53e94c50c156ebf263
Christine Yu <christine.tc.yu@gmail.com>
2014-03-17 07:53:10 -0700

    inital

---
+++ www/js/controllers.js
@@ -0,0 +1,20 @@
+angular.module('contac.controllers', [])
+.controller('LenseIndexCrl', function($scope, ContacService) {
+	$scope.lenses = ContacService.all();
+});
+
+angular.module('starter.controllers', [])
+
+
+// A simple controller that fetches a list of data from a service
+.controller('PetIndexCtrl', function($scope, PetService) {
+  // "Pets" is a service returning mock data (services.js)
+  $scope.pets = PetService.all();
+})
+
+
+// A simple controller that shows a tapped item's data
+.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
+  // "Pets" is a service returning mock data (services.js)
+  $scope.pet = PetService.get($stateParams.petId);
+});
