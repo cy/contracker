da7e70d999f061ec699325aef06c138b0a62ae1b
Christine Yu <christine.tc.yu@gmail.com>
2014-03-17 21:14:23 -0700

    customize a bit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,21 @@
 angular.module('contac.controllers', [])
-.controller('LenseIndexCrl', function($scope, ContacService) {
+.controller('LensesIndexCtrl', function($scope, ContacService) {
 	$scope.lenses = ContacService.all();
+	$scope.hello = 'world';
 });
 
-angular.module('starter.controllers', [])
+/*angular.module('starter.controllers', [])*/
 
 
-// A simple controller that fetches a list of data from a service
-.controller('PetIndexCtrl', function($scope, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.pets = PetService.all();
-})
+//// A simple controller that fetches a list of data from a service
+//.controller('PetIndexCtrl', function($scope, PetService) {
+  //// "Pets" is a service returning mock data (services.js)
+  //$scope.pets = PetService.all();
+//})
 
 
-// A simple controller that shows a tapped item's data
-.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.pet = PetService.get($stateParams.petId);
-});
+//// A simple controller that shows a tapped item's data
+//.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
+  //// "Pets" is a service returning mock data (services.js)
+  //$scope.pet = PetService.get($stateParams.petId);
+/*});*/
