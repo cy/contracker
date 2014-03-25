c3a3c57b04ebf455f8bed3560509eeb00b6bcec0
Christine Yu <christine.tc.yu@gmail.com>
2014-03-24 20:28:48 -0700

    month selector

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,7 +5,7 @@ angular.module('contac.controllers', [])
 .controller('LenseDetailCtrl', function($scope, $stateParams, ContacService) {
 	$scope.lense = ContacService.get($stateParams.id);
 })
-.controller('LenseNewCtrl', function($scope, $location, ContacService) {
+.controller('LenseNewCtrl', function($scope, $location, $ionicModal, ContacService) {
 	$scope.newLense = ContacService.makeNew();
 	$scope.addDays = function(days) {
 		$scope.newLense.schedule += days;
@@ -23,20 +23,38 @@ angular.module('contac.controllers', [])
 		ContacService.add($scope.newLense);
 		$location.path("/tab/lenses/");
 	}
+
+  // Load the modal from the given template URL
+  $ionicModal.fromTemplateUrl('modal.html', function(modal) {
+    $scope.modal = modal;
+  }, {
+    // Use our scope for the scope of the modal to keep it simple
+    scope: $scope,
+    // The animation we want to use for the modal entrance
+    animation: 'slide-in-up'
+  });
+
+	//$scope.months = {'January': 0, 'February': 1, 'March': 2, 'April': 3, 'May': 4, 'June': 5, 'July': 6, 'August': 7, 'September': 8, 'October': 9, 'November': 10, 'December': 11};
+
+	$scope.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+	$scope.pickMonth = function(pickedMonth) {
+		console.log("picked " + pickedMonth);
+		$scope.newLense.openedDate.setMonth(pickedMonth);
+		$scope.closeModal();
+	}
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  //Be sure to cleanup the modal
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
 });
 
-/*angular.module('starter.controllers', [])*/
-
-
-//// A simple controller that fetches a list of data from a service
-//.controller('PetIndexCtrl', function($scope, PetService) {
-  //// "Pets" is a service returning mock data (services.js)
-  //$scope.pets = PetService.all();
-//})
-
-
-//// A simple controller that shows a tapped item's data
-//.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
-  //// "Pets" is a service returning mock data (services.js)
-  //$scope.pet = PetService.get($stateParams.petId);
-/*});*/
--- www/js/services.js
+++ www/js/services.js
@@ -13,40 +13,12 @@ angular.module('contac.services', [])
 			lenses.push(lense);
 		},
 		get: function(id) {
-			console.log("get!");
 			return lenses[id];
 		},
 		makeNew: function() {
-			console.log("makenew!".concat(new Date()));
 			var newLense = { openedDate: new Date(), side: 'left', schedule: 14};
 			return newLense;
 		}
 	}
 });
 
-/*angular.module('starter.services', [])*/
-
-/**
- * A simple example service that returns some data.
- */
-//.factory('PetService', function() {
-  //// Might use a resource here that returns a JSON array
-
-  //// Some fake testing data
-  //var pets = [
-    //{ id: 0, title: 'Cats', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
-    //{ id: 1, title: 'Dogs', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
-    //{ id: 2, title: 'Turtles', description: 'Everyone likes turtles.' },
-    //{ id: 3, title: 'Sharks', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' }
-  //];
-
-  //return {
-    //all: function() {
-      //return pets;
-    //},
-    //get: function(petId) {
-      //// Simple index lookup
-      //return pets[petId];
-    //}
-  //}
-/*});*/
